a0e0c2c1070e450f246054320d29e8a75d53d3fe
AlikRodriguez <74626882+AlikRodriguez@users.noreply.github.com>
2021-01-24 22:47:34 -0600

    initial commit

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,44 +1,50 @@
-import './PokeCard.css';
-import React, { useState } from 'react';
-import { mockPokemonData } from '../mock/pokeData';
+import "./PokeCard.css";
+import React, { useState } from "react";
+import { mockPokemonData } from "../mock/pokeData";
 
 function getData() {
-    return Promise.resolve( mockPokemonData );
+  return Promise.resolve(mockPokemonData);
 }
 
 const initialPokemonState = {
-    name: '',
-    sprites: {
-        front_default: '',
-        front_shiny: ''
-    },
-    video: ''
+  name: "",
+  sprites: {
+    front_default: "",
+    front_shiny: ""
+  },
+  video: ""
 };
 
 function PokeCard() {
-    const [pokemon, setPokemon] = useState(initialPokemonState);
+  const [pokemon, setPokemon] = useState(initialPokemonState);
 
-    getData()
-        .then(pokemon => {
-             setPokemon(pokemon)
-        });
+  getData().then((pokemon) => {
+    setPokemon(pokemon);
+  });
 
-    return (
-        <div className="poke-card"> 
-            <div className="pokemon-header">
-                <h1 className="pokemon-name">{pokemon.name}</h1>
-            </div>
-            <div className="pokemon-images">
-                <img src={pokemon.sprites.front_default} alt="default" />
-                <img src={pokemon.sprites.front_shiny} alt="shiny" />
-            </div>
-            <div className="pokemon-footer">
-                <a className="pokemon-video" href={pokemon.video} target="_blank" rel="noopener noreferrer">{pokemon.name} video</a>
-            </div>
-        </div>
-    );
-    
+  return (
+    <div className="poke-card">
+      <div className="pokemon-header">
+        <h1 className="pokemon-name">{pokemon.name}</h1>
+      </div>
+
+      <div className="pokemon-images">
+        <img src={pokemon.sprites.front_default} alt="default" />
+        <img src={pokemon.sprites.front_shiny} alt="shiny" />
+      </div>
+
+      <div className="pokemon-footer">
+        <a
+          className="pokemon-video"
+          href={pokemon.video}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {pokemon.name} video
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default PokeCard;
-
