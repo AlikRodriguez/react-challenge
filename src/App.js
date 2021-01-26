006ef6be83a1dde12b782c2ca3f9ab7795e4aca8
Alik Rodriguez <irwin.rodriguez@wizeline.com>
2021-01-25 18:10:17 -0600

    refactor App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
+import "./styles.css";
 import React, {useState} from "react";
 import PokeCard from "./components/PokeCard";
-import "./styles.css";
-import { getData } from "./api/PokeAPI";
+import { getPokemon } from "./api/PokeAPI";
 
+const initialState = {
+    name: '',
+    video: '',
+    sprites: { front_default: '', front_shiny: '' }
+}
 
 export default function App() {
 
-    let [pokemon, setPokemon] = useState({name: '', video: '', sprites: {}})
+    let [pokemon, setPokemon] = useState(initialState)
+    getPokemon().then(setPokemon)
 
-    getData()
-        .then((pokemon) => {
-            setPokemon(pokemon)
-    })
-
-    return (
-        <div className="App">
-           <PokeCard pokemon={pokemon}/>
-        </div>
-  );
+    return <div className="App"> <PokeCard pokemon={pokemon}/> </div>
 }
